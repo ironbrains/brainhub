79cb4fd6c56111785e624c6c49b5529359869588
fatbulat <fatbulat5@gmail.com>
2017-02-18 22:12:38 +0300

    Add error views for sessions

--- web/views/session_view.ex
+++ web/views/session_view.ex
@@ -7,4 +7,16 @@ defmodule Brainhub.SessionView do
       user: user
     }
   end
+
+  def render("error.json", _) do
+    %{ error: "Invalid email or password" }
+  end
+
+  def render("delete.json", _) do
+    %{ ok: true }
+  end
+
+  def render("forbidden.json", %{error: error}) do
+    %{ error: error }
+  end
 end
