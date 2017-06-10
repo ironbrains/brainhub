836e4a357f7ae6d3d6f3b81bd17fa68528dba6ea
Bulat F <write2bulat@gmail.com>
2017-06-10 20:20:51 +0300

    Improve TimeEntry model

--- test/models/time_entry_test.exs
+++ test/models/time_entry_test.exs
@@ -3,15 +3,25 @@ defmodule Brainhub.TimeEntryTest do
 
   alias Brainhub.TimeEntry
 
-  @valid_attrs %{project_id: 1, user_id: 1, duration: 100, start_at: Calendar.DateTime.now_utc}
+  import GoodTimes.Convert
+  import GoodTimes.Date
+
+  @valid_attrs %{project_id: 1, user_id: 1, duration: 100, start_at: Ecto.DateTime.utc}
   @invalid_attrs %{}
 
   setup do
     user = insert :user
     company = insert :company
     project = insert :project, company: company
+    {:ok, yesterday} = Calendar.DateTime.from_erl(yesterday() |> from_date, "Etc/UTC")
+    today = Ecto.DateTime.utc
+
+    insert :time_entry, start_at: yesterday, duration: 3000, user: user
+    insert :time_entry, start_at: today, duration: 1000
+    insert :time_entry, start_at: today, duration: 2000, user: user
+
     insert :employment, user: user, company: company
-    {:ok, user: user, project: project}
+    {:ok, user: user, project: project, yesterday: yesterday, today: today}
   end
 
   describe "changeset" do
@@ -24,6 +34,36 @@ defmodule Brainhub.TimeEntryTest do
     test "with invalid attributes" do
       changeset = TimeEntry.changeset(%TimeEntry{}, @invalid_attrs)
       refute changeset.valid?
+    end
+  end
+
+  describe "duration_for_date/2" do
+    test "with Ecto.DateTime as param" , %{today: today} do
+      assert TimeEntry.duration_for_date(TimeEntry, today) == 3000
+    end
+  end
+
+  describe "created_by/1" do
+    test "with Brainhub.User as param", %{user: user} do
+      entries = TimeEntry.created_by user
+      assert length(entries) == 2
+    end
+
+    test "with id (interger) as param", %{user: user} do
+      entries = TimeEntry.created_by user.id
+      assert length(entries) == 2
+    end
+  end
+
+  describe "duration_for/2" do
+    test "returns duration of user entries by date", %{user: user, today: today} do
+      duration = TimeEntry.duration_for(user, today)
+      assert duration == 2000
+    end
+
+    test "returns duration of user entries when date is nil", %{user: user} do
+      duration = TimeEntry.duration_for(user, nil)
+      assert duration == 5000
     end
   end
 end
--- web/models/time_entry.ex
+++ web/models/time_entry.ex
@@ -24,15 +24,37 @@ defmodule Brainhub.TimeEntry do
     |> assoc_constraint(:user)
   end
 
-  def duration_for_date(%Ecto.DateTime{year: year, month: month, day: day}) do
-    duration_for_date %Ecto.Date{year: year, month: month, day: day}
+  def duration_for_date(query, %Ecto.DateTime{year: year, month: month, day: day}) do
+    duration_for_date query, %Ecto.Date{year: year, month: month, day: day}
   end
 
-  def duration_for_date(date) do
-    start_of_day = DateTime.from_date_and_time date,  Time.from_erl({0, 0, 0})
+  def duration_for_date(query, nil) do
+    Brainhub.Repo.aggregate(query, :sum, :duration)
+  end
+
+  def duration_for_date(query, date) do
+    start_of_day = DateTime.from_date_and_time date, Time.from_erl({0, 0, 0})
     end_of_day = DateTime.from_date_and_time date, Time.from_erl({23, 59, 59})
-    Brainhub.TimeEntry
+    query
     |> where([e], e.start_at >= ^start_of_day and e.start_at <= ^end_of_day)
     |> Brainhub.Repo.aggregate(:sum, :duration)
+  end
+
+  def created_by(%Brainhub.User{id: user_id}), do: created_by user_id
+
+  def created_by(user_id) do
+    Brainhub.TimeEntry
+    |> where([t], t.user_id == ^user_id)
+    |> Brainhub.Repo.all
+  end
+
+  def duration_for(user), do: duration_for user, nil
+
+  def duration_for(%Brainhub.User{id: user_id}, date), do: duration_for user_id, date
+
+  def duration_for(user_id, date) do
+    Brainhub.TimeEntry
+    |> where([t], t.user_id == ^user_id)
+    |> Brainhub.TimeEntry.duration_for_date(date)
   end
 end
