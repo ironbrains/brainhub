bacbd2f3ae5c0faf3c3057c16d53d03047ac974a
Bulat F <write2bulat@gmail.com>
2017-10-28 22:37:31 +0300

    Remake styles and Landing page

--- assets/static/js/app/static-pages/index.component.js
+++ assets/static/js/app/static-pages/index.component.js
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'index',
-  template: require('./index.component.html.slim')
+  template: require('./index.component.html')
 })
 export class IndexComponent {
   constructor() {
